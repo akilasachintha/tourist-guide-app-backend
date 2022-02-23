7278a93539c3e34e266bf6f4aaac0e03e284ef2d
Akila Sachintha <akilasachintha@gmail.com>
2022-02-23 19:40:33 +0530

    Origin Main

---
+++ src/test/java/com/datapirates/touristguideappbackend/TouristGuideAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datapirates.touristguideappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
