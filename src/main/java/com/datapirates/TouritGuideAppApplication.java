116e0422624d1584439bd0aab0fad905d9a8ab89
Akila Sachintha <akilasachintha@gmail.com>
2022-02-23 16:14:07 +0530

    make it better

---
+++ src/main/java/com/datapirates/TouritGuideAppApplication.java
@@ -0,0 +1,13 @@
+package com.datapirates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouritGuideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouritGuideAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datapirates/TouritGuideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datapirates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouritGuideAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
