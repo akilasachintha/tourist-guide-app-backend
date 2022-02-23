b16bbe021932999f8012695605b270455608e952
Akila Sachintha <akilasachintha@gmail.com>
2022-02-23 19:26:51 +0530

    Add

---
+++ src/test/java/com/datapirates/touristguideapp/TouristGuideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datapirates.touristguideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
