5303fad27930be9852ee265f2da8de5f9491ef10
Akila Sachintha <akilasachintha@gmail.com>
2022-05-30 13:41:54 +0530

    Change Location Service

--- src/main/java/com/datapirates/touristguideapp/service/impl/LocationServiceImpl.java
+++ src/main/java/com/datapirates/touristguideapp/service/impl/LocationServiceImpl.java
@@ -2,75 +2,96 @@ package com.datapirates.touristguideapp.service.impl;
 
 import com.datapirates.touristguideapp.dto.responseDto.LocationLocationImageDTO;
 import com.datapirates.touristguideapp.entity.location.Location;
+import com.datapirates.touristguideapp.entity.location.LocationImage;
+import com.datapirates.touristguideapp.exception.ResourceNotFoundException;
+import com.datapirates.touristguideapp.repository.LocationRepository;
 import com.datapirates.touristguideapp.service.interfaces.LocationService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class LocationServiceImpl implements LocationService {
+
+    private final LocationRepository locationRepository;
+
     @Override
     public Location saveLocation(Location location) {
-        return null;
+        return locationRepository.save(location);
     }
 
     @Override
     public Location updateLocation(Long id, Location location) {
-        return null;
+        Location existingLocation = locationRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Location", "Id", id));
+
+        existingLocation.setLocationName(location.getLocationName());
+        existingLocation.setDistrict(location.getDistrict());
+        existingLocation.setTown(location.getTown());
+        existingLocation.setCategory(location.getCategory());
+        existingLocation.setDescription(location.getDescription());
+        existingLocation.setLocationImages(location.getLocationImages());
+        existingLocation.setDrivers(location.getDrivers());
+        existingLocation.setHotels(location.getHotels());
+
+        locationRepository.save(existingLocation);
+        return existingLocation;
     }
 
     @Override
     public String deleteLocation(Long id) {
-        return null;
+        Optional<Location> optionalLibrary = locationRepository.findById(id);
+        if (!optionalLibrary.isPresent()) {
+            return "Deletion Error";
+        }
+        locationRepository.delete(optionalLibrary.get());
+        return "Location Id " + id + " Deleted Successfully";
     }
 
     @Override
     public LocationLocationImageDTO getLocationById(Long id) {
-        return null;
+        //Normal Approach
+//        Optional<Location> location = locationRepository.findById(id);
+//
+//        if(location.isPresent()){
+//            LocationLocationImageDTO dto = location.;
+//            return location.get();
+//        }
+//        else {
+//            throw new ResourceNotFoundException("Location", "Id", id);
+//        }
+
+        //Lambda Expression Approach
+        Location existingLocation = locationRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Location", "Id", id));
+        return convertEntityToDto(existingLocation);
     }
 
     @Override
     public List<LocationLocationImageDTO> getAllLocations() {
-        return null;
+        return locationRepository.findAll().stream().map(this::convertEntityToDto).collect(Collectors.toList());
     }
 
-//    private final LocationRepository locationRepository;
-//
-//    @Override
-//    public Location saveLocation(Location location) {
-//        return locationRepository.save(location);
-//    }
-//
-//    @Override
-//    public String updateLocation(Long id, Location location) {
-//        Optional<Location> optionalLibrary = locationRepository.findById(id);
-//        if (!optionalLibrary.isPresent()) {
-//            return "Update Error";
-//        }
-//        location.setLocationId(optionalLibrary.get().getLocationId());
-//        locationRepository.save(location);
-//        return "Location Id " + id + " Updated Successfully";
-//    }
-//
-//    @Override
-//    public String deleteLocation(Long id) {
-//        Optional<Location> optionalLibrary = locationRepository.findById(id);
-//        if (!optionalLibrary.isPresent()) {
-//            return "Deletion Error";
-//        }
-//        locationRepository.delete(optionalLibrary.get());
-//        return "Location Id " + id + " Deleted Successfully";
-//    }
-//
-//    @Override
-//    public Optional<Location> getLocationById(Long id) {
-//        return locationRepository.findById(id);
-//    }
-//
-//    @Override
-//    public List<Location> getAllLocations() {
-//        return locationRepository.findAll();
-//    }
+    private LocationLocationImageDTO convertEntityToDto(Location location){
+        LocationLocationImageDTO locationLocationImageDTO = new LocationLocationImageDTO();
+
+        locationLocationImageDTO.setLocationId(location.getLocationId());
+        locationLocationImageDTO.setLocationName(location.getLocationName());
+        locationLocationImageDTO.setDistrict(location.getDistrict());
+        locationLocationImageDTO.setTown(location.getTown());
+        locationLocationImageDTO.setCategory(location.getCategory());
+        locationLocationImageDTO.setDescription(location.getDescription());
+
+        Set<LocationImage> locationImages= location.getLocationImages();
+        List<String> list = locationImages.stream().map(LocationImage::getUrl).collect(Collectors.toList());
+
+        locationLocationImageDTO.setUrls(list);
+
+        return locationLocationImageDTO;
+    }
 }
